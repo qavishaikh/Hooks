92465fad4c37f6ed461ad488b12bfb2385fec10b
Qavi <qavi.shaikh11@gmail.com>
2023-09-27 21:56:02 +0500

    Tables In React Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,65 +1,46 @@
 import React from 'react'
 import './App.css'
 
+import Table from 'react-bootstrap/Table';
+
 function App() {
+  const tableData = [
+    {id:1, name:"Qavi",age:19,city:"Hyderabad" },
+    {id:2, name:"Dani",age:59,city:"Hyderabad" },
+    {id:3, name:"Zain",age:79,city:"Dadu" },
+    {id:4, name:"Mano",age:59,city:"Nawabshah" },
+    {id:5, name:"Ali",age:30,city:"Dadu" },
+    {id:6, name:"Abdi",age:53,city:"Sakhar" },
+    {id:7, name:"Anus",age:32,city:"Hala" },
+    {id:8, name:"Ayat",age:80,city:"Paretabad" }
 
-  const names = ["Qavi","Hamdan","Ali","Hussain","Haram"]
-
-  const products = [{
-    id : 1,
-    name : "Procuct 1",
-    price : 200
-  },
-  {
-    id : 2,
-    name : "Procuct 2",
-    price : 350
-  },
-  {
-    id : 3,
-    name : "Procuct 3",
-    price : 300
-  },
-  {
-    id : 4,
-    name : "Procuct 4",
-    price : 400
-  },
-  {
-    id : 5,
-    name : "Procuct 5",
-    price : 500
-  }
-]
-
-const fruits = ["Apple","Mango","Orange","Grapes","pinepale"]
-const fruititems = fruits.map((fruit,index) => <li style={{"listStyle":"none",color:"orange"}} key={index}>{fruit}</li>);
- 
+  ]
+  
   return (
     <div className='App'>
-      <h1>Array Map Methods</h1>
-      {/* Array Map */}
-      <ul>
-        {
-          names.map((names,index) => (
-            <li style={{"listStyle":"none"}} key={index}>{names}</li>
-          ))
-        }
-      </ul>
-      {/* Object Map */}
-
-      <ul>
-        {
-          products.map((product) => (
-            <li style={{"listStyle":"none",color:"red"}} key={product.id}>{product.name}- ${product.price}</li>
-          ))
-        }
-  
-      </ul>
-
-      {/* Object Map */}
-
-      <ul>{fruititems}</ul>
+      <h1>Bootstrap Table in React</h1>
+      
+      <Table striped bordered hover>
+        <thead>
+          <th>Roll No</th>
+          <th>Name</th>
+          <th>Age</th>
+          <th>City</th>
+        </thead>
+        <tbody>
+          {
+            tableData.map((data) => (
+            data.city === "Hyderabad" ? (
+              <tr key={data.id}>
+                <td>{data.id}</td>
+                <td>{data.name}</td>
+                <td>{data.age}</td>
+                <td>{data.city}</td>
+              </tr> ) : null 
+            ))
+          }
+        </tbody>
+      </Table>
     </div>
   )
 }
